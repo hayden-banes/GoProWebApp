9476ece28a2fae9c3803032bde991ce164c30acb
Hayden <60787897+hayden-banes@users.noreply.github.com>
2024-07-16 10:06:32 +0100

    Update settings.py

--- GoProWebApp/GoProWebApp/settings.py
+++ GoProWebApp/GoProWebApp/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%7k67nafcsx6diqh94ken^1#e7)55y7eb_+qafmjbbl##hx86m'
+SECRET_KEY = os.environ['GOPRO_WEBAPP_SECRET_KEY']
+SECRET_KEY_FALLBACKS = [
+    os.environ["OLD_GOPRO_WEBAPP_SECRET_KEY"],
+]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
